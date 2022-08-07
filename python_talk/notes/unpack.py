870d718e8c536d72cf66371921d10fe9098b1ce0
benkaan001 <benkaan001@gmail.com>
2022-08-07 13:22:15 -0500

    add unpack / update zip

---
+++ python_talk/notes/unpack.py
@@ -0,0 +1,23 @@
+'''
+a, b = 1, 2        -> a=1 & b=2
+a, b = 1, 2, 3     -> ValueERROR 
+a, b, c = 1,2      -> ValueEROOR
+
+'''
+
+# Use _ for variables that will not be used
+
+calories= 100, 200
+apple, _ = calories 
+print(apple) #100
+
+calories = 100, 200, 300, 400, 500
+_, banana, mango, *etc = calories
+print(banana) # 200
+print(mango) # 300
+print(etc) # [400,500]
+
+apple, banana, *etc, durian = calories
+print(durian) # 500
+print(etc) # [300,400]
+
--- python_talk/notes/zip.py
+++ python_talk/notes/zip.py
@@ -6,10 +6,24 @@ universes = ['Marvel', 'DC','Marvel', 'DC']
 
 for name,hero,universe in zip(names,heroes,universes):
     print(f"{name} is actually {hero} from {universe}")
-
-'''
+    
+    '''
     Peter Parker is actually Spiderman from Marvel
     Clark Kent is actually Superman from DC
     Wade Wilson is actually Deadpool from Marvel
     Bruce Wayne is actually Batman from DC
-'''
+    
+    '''
+
+# Not unpacking each value will result in a tuple
+
+for value in zip(names,heroes,universes):
+    print(value)
+    
+    '''
+    ('Peter Parker', 'Spiderman', 'Marvel')
+    ('Clark Kent', 'Superman', 'DC')
+    ('Wade Wilson', 'Deadpool', 'Marvel')
+    ('Bruce Wayne', 'Batman', 'DC')
+    
+    '''
