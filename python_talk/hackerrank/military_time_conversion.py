77a92c22d72c6275ef9a1d92c92a7e9b4ae8403b
benkaan001 <benkaan001@gmail.com>
2022-10-09 12:56:26 -0500

    update military time conversion

--- python_talk/hackerrank/military_time_conversion.py
+++ python_talk/hackerrank/military_time_conversion.py
@@ -7,7 +7,7 @@ second = input_str.split(":")[2][0:2] # '20'
 am_pm = input_str.split(":")[2][2:] # 'AM'
 
 if am_pm == "PM": 
-    hour = int(hour) + 12
+    hour = str(int(hour) + 12)
 elif am_pm == "AM" and hour == "12": 
     hour = "00" # '00'
 
