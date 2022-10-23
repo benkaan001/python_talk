73aa06f65c1c55a9b636d64ef7ad51cd88854a0c
benkaan001 <benkaan001@gmail.com>
2022-10-23 00:00:07 -0500

    add pass by assignment

--- python_talk/notes/scope/01_pass_by_assignment.py
+++ python_talk/notes/scope/01_pass_by_assignment.py
@@ -1,19 +1,32 @@
-'''
-    immutable      ->    pass by value
-    mutable        ->    pass by reference
+"""
+Pass by reference -> 
+    
+    argument that you are passing to a funciton is a reference to a variable
+    that already exists in the memory rather than an independent copy of that
+    argument
 
-'''
-a = 1
-b = [10]
+Pass by value ->
 
-def func(a,b):
-    a+=1
-    b+=[1]
-    return print(a,b, sep=", ") 
+    arguments that you pass become independent copies of the original values.
+"""
 
-func(a, b) # 2, [10,1]
+# Python passes arguments neither by reference nor by value, but by assignment.
+def main():
+    n = 9001
+    print(f"Initial address of n: {id(n)}")
+    increment(n)
+    print(f"  Final address of n: {id(n)}")
+def increment(x):
+    print(f"Initial address of x: {id(x)}")
+    x += 1
+    print(f"  Final address of x: {id(x)}")
 
-print(a) # 1
-print(b) # [10, 1]
+main()
 
-print(a in b) # True 
+"""
+Initial address of n: 140562586057840
+Initial address of x: 140562586057840
+  Final address of x: 140562586057968
+  Final address of n: 140562586057840
+
+"""
