c7558ec2dee08e29cec7b33c0a855aed2a2a98ac
benkaan001 <benkaan001@gmail.com>
2022-11-16 00:54:08 -0600

    add hackerrank prime twins

--- python_talk/hackerrank/prime_twins.py
+++ python_talk/hackerrank/prime_twins.py
@@ -7,31 +7,53 @@ The first twin primes are {3,5}, {5,7}, {11,13} and {17,19}.
 num= 31
 
 
-def is_prime(num):
+def isprime(num):
     
     for i in range(2,num):
-        if(num  %i == 0):
+        if(num  % i == 0):
             return False
     
     return True
 
-print(is_prime(31)) # true
+# test isprime
+# print(isprime(31)) # True
 
-prime_nums=[]
-for i in range(2,num):
-    if is_prime(i):
-        prime_nums.append(i)
 
-print(prime_nums)
 
-#[2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
 
-def generate_twins(end):
-    output=""
-    for i in range(2,end):
-        j=i+2
-        if(is_prime(i) and is_prime(j)):
-            output+=f'{i}<>{j},'
-    return output.strip(',')
+def get_twins():
+    
+    prime_nums=[i for i in range(2, num) if isprime(i)] # [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+    output = []
+    
+    for i in range(len(prime_nums) -1):
+        if prime_nums[i+1] - prime_nums[i] == 2:
+            output.append( ( str(prime_nums[i]) + ":" + str(prime_nums[i+1]) ) )
+            
+    return output
+"""
+output = [] 
 
-print(generate_twins(20))
+i = 0 | i = 1            | i = 2                   | i = 3 | i = 4                            | i = 5 | i = 6                                     | i = 7 | i = 8 
+      |                  |                         |       |                                  |       |                                           |       | 
+      | output = ['3:5'] | output = ['3:5', '5:7'] |       | output = ['3:5', '5:7', '11:13'] |       | output = ['3:5', '5:7', '11:13', '17:19'] |       | 
+
+"""
+
+output = get_twins() # ['3:5', '5:7', '11:13', '17:19']
+
+final_output = ",".join(i for i in output)
+
+print(final_output) # 3:5,5:7,11:13,17:19
+            
+
+# def generate_twins(end):
+#     output=""
+#     for i in range(2,end):
+#         j=i+2
+#         if(isprime(i) and isprime(j)):
+#             output+=f'{i}<>{j},' #  output = '3<>5,5<>7,11<>13,17<>19,' 
+#     return output.strip(',')
+#
+# print(generate_twins(20)) # 3<>5,5<>7,11<>13,17<>19
+
