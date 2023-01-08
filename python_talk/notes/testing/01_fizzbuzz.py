12990154cd3de42dd3c2a1c67e641ecb0fac9583
benkaan001 <benkaan001@gmail.com>
2023-01-08 12:46:55 -0600

    add testing

---
+++ python_talk/notes/testing/01_fizzbuzz.py
@@ -0,0 +1,15 @@
+def fizzBuzz(num: int) -> str:
+    if num % 5 == 0 and num % 3 == 0:
+        return "FizzBuzz"
+    elif num % 5 == 0:
+        return "Buzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    else:
+        return str(num)
+
+
+expected = "FizzBuzz"
+result = fizzBuzz(15)
+assert (type(result) == str), f"Expected actual to be of string type, but found {type(result)}"
+assert result == expected, f"Expected {expected}, but found {result} "
